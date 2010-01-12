e43092a0cd03e3a382c8f0c31421d9073009535c
Yauhen Kharuzhy <jekhor@gmail.com>
2010-01-12 19:23:12 +0200

    test_xml: Add Database.rb to requires
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- test/test_xml.rb
+++ test/test_xml.rb
@@ -1,5 +1,6 @@
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require File.join(File.dirname(__FILE__), '..', 'lib', 'OSM', 'objects')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'OSM', 'Database')
 require 'test/unit'
 require 'rexml/document'
 require 'rubygems'
